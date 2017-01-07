e566ff0c9e664df115bbb700d37039377fa1960e
Phil Myers <prm5031@gmail.com>
2017-01-07 15:23:28 -0500

    Create brew science script for opencv

---
+++ os/install/macos/brew-science.sh
@@ -0,0 +1,11 @@
+# Install Science
+
+brew tap homebrew/science
+
+# Install packages
+
+apps=(
+    opencv3
+)
+
+brew install "${apps[@]}"
--- os/install/macos/main.sh
+++ os/install/macos/main.sh
@@ -4,5 +4,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 ./xcode.sh
 ./brew.sh
 ./brew-cask.sh
+./brew-science.sh
 ./antigen.sh
 ./dock.sh
